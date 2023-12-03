086c387110beb12518de850db49521966f58555e
brayans22 <bsaiago@fi.uba.ar>
2023-12-04 00:01:09 +0100

    add dependency react-router-dom

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './components/app/App.jsx'
-import Header from './components/header/Header.jsx'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
